c5fcc9d0e48db63fac762de1e427d1abb219eca7
Riley Grant <grantrileyh@gmail.com>
2024-12-01 17:33:59 -0500

    refactor: rework day 1 part 1 and 2 solutions

--- src/day1.rs
+++ src/day1.rs
@@ -4,56 +4,50 @@ mod lib;
 fn shape_input(filepath: &str) -> (Vec<i32>, Vec<i32>) {
     let contents = lib::read_input(format!("input/{}", filepath));
 
-    let mut left_numbers = Vec::new();
-    let mut right_numbers = Vec::new();
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = contents
+        .iter()
+        .filter_map(|line| {
+            let nums: Vec<i32> = line.split_whitespace()
+                .filter_map(|n| n.parse().ok())
+                .collect();
+            match nums[..] {
+                [left, right] => Some((left, right)),
+                _ => None
+            }   
+        })
+        .unzip();
 
-    for line in contents.iter() {
-        let mut numbers = line.split_whitespace();
-
-        if let Some(left) = numbers.next() {
-            left_numbers.push(left.parse::<i32>().unwrap());
-        }
-
-        if let Some(right) = numbers.next() {
-            right_numbers.push(right.parse::<i32>().unwrap());
-        }
-    }
-
-    left_numbers.sort();
-    right_numbers.sort();
-
-    return (left_numbers, right_numbers)
+    left.sort();
+    right.sort();
+    (left, right)
 }
 
 
 
 pub fn part_1(path: &str) -> String {
-    let (left_numbers, right_numbers) = shape_input(path);
-
-    let mut sum = 0;
-    for (i, left_value) in left_numbers.iter().enumerate() {
-        let right_value = right_numbers[i];
-        let difference = (left_value - right_value).abs();
-        sum = sum + difference;
-    }
+    let (left, right) = shape_input(path);
+    
+    let sum: i32 = left.iter()
+        .zip(&right)
+        .map(|(l, r)| (l - r).abs())
+        .sum();
 
     return sum.to_string()
 }
 
+
+
 pub fn part_2(path: &str) -> String {
-    let (left_numbers, right_numbers) = shape_input(path);
+    let (left, right) = shape_input(path);
 
-    let mut sum = 0;
-
-    for left_value in left_numbers.iter() {
-        let mut right_count = 0;
-        for right_value in right_numbers.iter() {
-            if left_value == right_value {
-                right_count += 1;
-            }
-        }
-        sum = sum + left_value * right_count;
-    }
+    let sum: i32 = left.iter()
+        .map(|l_value| {
+            let r_count: i32 = right.iter()
+                .filter(|&r_value| r_value == l_value)
+                .count() as i32;
+            l_value * r_count
+        })
+        .sum();
 
     return sum.to_string()
 }
@@ -64,6 +58,7 @@ pub fn part_2(path: &str) -> String {
 mod tests {
     use super::*;
 
+
     #[test]
     fn test_day_1_part_1() {
 
@@ -73,6 +68,7 @@ mod tests {
         let full_result = part_1("day01.txt");
         assert_eq!(full_result, "1834060");
     }
+
 
     #[test]
     fn test_day_1_part_2() {
