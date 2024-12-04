0497cef081abef390e6eec451cf5bbbc303e9124
Riley Grant <grantrileyh@gmail.com>
2024-12-04 10:34:50 -0600

    fix: make read_input ignore empty lines

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ use std::fs;
 #[allow(dead_code)]
 pub fn read_input(filepath: String) -> Vec<String> {
     let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
-    let m_lines: Vec<String> = contents.lines().map(|x| x.to_owned()).collect();
+    let m_lines: Vec<String> = contents.lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.to_owned())
+        .collect();
     return m_lines
 }
 
