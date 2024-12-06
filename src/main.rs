9e446709375f35e79b490482753943f9085d66d8
Riley Grant <grantrileyh@gmail.com>
2024-12-06 09:04:22 -0600

    chore: standardize day digits

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::env;
 
 #[path = "./lib.rs"]
 mod lib;
-mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
 
 fn main() {
     println!("\n\n===================");
@@ -25,31 +25,31 @@ fn main() {
     };
 
     for day in days {
-        println!("\n=== Day {:02}===", day);
+        println!("\n=== Day {:02} ===", day.parse::<i32>().unwrap());
         match day.as_str() {
             "1" => {
-                lib::print_output("1", day1::part_1("day01.txt"));
-                lib::print_output("2", day1::part_2("day01.txt"));
+                lib::print_output("1", day01::part_1("day01.txt"));
+                lib::print_output("2", day01::part_2("day01.txt"));
             }
             "2" => {
-                lib::print_output("1", day2::part_1("day02.txt"));
-                lib::print_output("2", day2::part_2("day02.txt"));
+                lib::print_output("1", day02::part_1("day02.txt"));
+                lib::print_output("2", day02::part_2("day02.txt"));
             }
             "3" => {
-                lib::print_output("1", day3::part_1("day03.txt"));
-                lib::print_output("2", day3::part_2("day03.txt"));
+                lib::print_output("1", day03::part_1("day03.txt"));
+                lib::print_output("2", day03::part_2("day03.txt"));
             }
             "4" => {
-                lib::print_output("1", day4::part_1("day04.txt"));
-                lib::print_output("2", day4::part_2("day04.txt"));
+                lib::print_output("1", day04::part_1("day04.txt"));
+                lib::print_output("2", day04::part_2("day04.txt"));
             }
             "5" => {
-                lib::print_output("1", day5::part_1("day05.txt"));
-                lib::print_output("2", day5::part_2("day05.txt"));
+                lib::print_output("1", day05::part_1("day05.txt"));
+                lib::print_output("2", day05::part_2("day05.txt"));
             }
             "6" => {
-                lib::print_output("1", day6::part_1("day06.txt"));
-                lib::print_output("2", day6::part_2("day06.txt"));
+                lib::print_output("1", day06::part_1("day06.txt"));
+                lib::print_output("2", day06::part_2("day06.txt"));
             }
             _ => println!(" -- not implemented yet"),
         }
