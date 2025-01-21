1dc55d29f3c8d0f2090ba30697668ed6b6ded4bd
Riley Grant <grantrileyh@gmail.com>
2025-01-21 11:42:49 -0600

    refactor: improve day 16 performance

--- src/day16.rs
+++ src/day16.rs
@@ -1,10 +1,30 @@
 #[path = "./lib.rs"]
 mod lib;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 type Num = i32;
 type Position = (usize, usize);
 
-use std::collections::{HashMap, HashSet};
+#[derive(Eq, PartialEq)]
+struct State {
+    cost: i32,
+    pos: (usize, usize),
+    dir: char,
+    path: Vec<Position>,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 fn find_char(grid: &Vec<Vec<char>>, to_find: char) -> (usize, usize) {
     for (i, row) in grid.iter().enumerate() {
@@ -49,49 +69,57 @@ fn is_valid_move(maze: &Vec<Vec<char>>, pos: Position) -> bool {
     maze[pos.0][pos.1] != '#'
 }
 
-fn dfs(maze: Vec<Vec<char>>) -> (Num, usize) {
-    let mut stack = Vec::new();
+fn dijkstras(maze: Vec<Vec<char>>) -> (Num, usize) {
+    let mut queue = BinaryHeap::new();
     let mut costs: HashMap<((usize, usize), char), i32> = HashMap::new();
     let mut best_cost = Num::MAX;
-
     let mut cost_and_path: HashMap<Num, Vec<Vec<Position>>> = HashMap::new();
 
     let start_pos = find_char(&maze, 'S');
-
     let init_path = vec![start_pos];
-    stack.push((start_pos, '>', 0, init_path));
 
-    while let Some(current) = stack.pop() {
-        if current.2 > best_cost {
+    queue.push(State {
+        cost: 0,
+        pos: start_pos,
+        dir: '>',
+        path: init_path,
+    });
+
+    while let Some(current) = queue.pop() {
+        if current.cost > best_cost {
             continue;
         }
 
-        if let Some(&prev_cost) = costs.get(&(current.0, current.1)) {
-            // TODO: this is awful
-            if current.2 >= prev_cost + 1000 {
+        if let Some(&prev_cost) = costs.get(&(current.pos, current.dir)) {
+            if current.cost >= prev_cost + 1000 {
                 continue;
             }
         }
 
-        costs.insert((current.0, current.1), current.2);
+        costs.insert((current.pos, current.dir), current.cost);
 
-        if maze[current.0 .0][current.0 .1] == 'E' {
-            best_cost = best_cost.min(current.2);
+        if maze[current.pos.0][current.pos.1] == 'E' {
+            best_cost = best_cost.min(current.cost);
             cost_and_path
-                .entry(current.2)
+                .entry(current.cost)
                 .or_insert_with(Vec::new)
-                .push(current.3.clone());
+                .push(current.path.clone());
             continue;
         }
 
-        for next_dir in get_possible_directions(current.1) {
-            let next_pos = get_next_position(current.0, next_dir);
+        for next_dir in get_possible_directions(current.dir) {
+            let next_pos = get_next_position(current.pos, next_dir);
             if is_valid_move(&maze, next_pos) {
-                let move_cost = if next_dir == current.1 { 1 } else { 1001 };
-
-                let mut new_path = current.3.clone();
+                let move_cost = if next_dir == current.dir { 1 } else { 1001 };
+                let mut new_path = current.path.clone();
                 new_path.push(next_pos);
-                stack.push((next_pos, next_dir, current.2 + move_cost, new_path));
+
+                queue.push(State {
+                    cost: current.cost + move_cost,
+                    pos: next_pos,
+                    dir: next_dir,
+                    path: new_path,
+                });
             }
         }
     }
@@ -109,14 +137,14 @@ fn dfs(maze: Vec<Vec<char>>) -> (Num, usize) {
 
 pub fn part_1(path: &str) -> String {
     let grid = lib::create_grid(path);
-    let tupl = dfs(grid);
+    let tupl = dijkstras(grid);
 
     tupl.0.to_string()
 }
 
 pub fn part_2(path: &str) -> String {
     let grid = lib::create_grid(path);
-    let tupl = dfs(grid);
+    let tupl = dijkstras(grid);
     tupl.1.to_string()
 }
 
